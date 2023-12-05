f0eafd10bd068c5a62c0a098586b3e35f6429d5c
QuestionableJay <143842477+QuestionableJay@users.noreply.github.com>
2023-12-04 21:32:52 -0500

    .

--- src/Tile Layers/map.py
+++ src/Tile Layers/map.py
@@ -3,6 +3,6 @@ from src.world import World
 from src.Controller import Controller
 
 world = World(map_images)
-world.draw(self.screne)
+world.draw(self.scene)
 
 map_image = pg.image.load('BTD MAP/Map.png').convert_alpha() 
