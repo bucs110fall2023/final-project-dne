970dd83f578bd1e86991ddceb47830823b857f44
SJMcStay <scottjmcstay@gmail.com>
2023-12-05 17:24:38 -0500

    image updated

--- src/Constants.py
+++ src/Constants.py
@@ -3,7 +3,7 @@ class Constants:
     #file location of according images 
     MAPIMAGE = "assets/Map.png"
     MENUIMAGE = "assets/Menu.jpg"
-    ENEMYIMAGE = "assets/enemy.jpg"
+    ENEMYIMAGE = "assets/Balloon.png"
     MONKEYIMAGE =  "assets/Monkey.jpg"
     BUTTONIMAGE = "assets/Basic_red_dot.png"
    
